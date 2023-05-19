089d783f06090f4f6b422758a5abf750d71c2448
Miljan Milidrag <miljan.milidrag@protonmail.com>
2023-05-19 18:16:13 +0200

    Initail commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>WEB 2.5 Trivia</h1>
+    </div>
+  );
+}
+
+export default App;
